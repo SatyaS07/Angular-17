8e3815934067bb689228454fac3f0c4f3c8da8bd
SatyaS07 <satyasagarmohanta@gmail.com>
2024-03-15 10:22:55 +0530

    update

---
+++ angular/components-movie/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'components-movie';
+  img1="https://occ-0-2610-3646.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABaSul_dyl7Vp3dMc_7KoET72knNATy8bgAjoJj9KpYYeqK3RaxRd2-S1pIqDUVsb74gPp5fiAYNFV5iNq-LYmTLFuZBVn3cOmyRhtJL0bHAqejMOv3T0kslLRDKvH6WujDZd.jpg?r=4fc"
+  img2="https://occ-0-2610-3646.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABQ8UCIw9Po1eDYGwhQvkJkwpZqn2dApkBhXJnTKu-e-tF-AOkcfYzVpDBlJKGI2vNAuEeoAropV_M0j_FVHgRNXi6C9MdaMDqU1esuY-RcP7Zm0u560UV8bJJBkF72G7yC_K.jpg?r=088"
+  img3="https://occ-0-2610-3646.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABYwIlfSntNrUdgcUZavTwGFTHGM1v9dlVxc9nuJ6Ge-8LWpH5sXGXlYKkDjxPJFYdpphlCgZZQ3VOtD4ee_gG8ndVQ__eFBQfa4rf4IX1TOOA936KM0q8UkglsIOW5n6h7Oa.jpg?r=257"
+  img4="https://occ-0-2610-3646.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABZS3AP64J4immWPo4qapDB_xFrMvNdBEiLW-1fc99mdQ36iOq0t9uxCehFenm9nx7E6z2KWP8EU9cq0BHK_l_F_7YDP_3N_QnrG8EJR8uEo9ADSqC2qFLP1hov1ytT_b29Gw.jpg?r=7f6"
+  img5="https://occ-0-2610-3646.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABdzwzlCOIvQKcEFJnQOuS3LvB-OMRgzvrnPjSfujC5KXU7ldNs6PZgsacbr2eAe8nV4XLSHjP6PZJAYJey2LDoR00bviEKJxkJyiqUO0EGOroED7vvOOGAAyrRYMKfUGvx8W.jpg?r=883"
+}
